8bf781a002f7c4059f19fa450800281522b7e0b3
Michael Luster <moluster@email.uark.edu>
2023-05-08 23:08:46 -0500

    Using a list from File to currently cacualte Net Profit

---
+++ src/main/java/com/netprofitcalculator/netprofitcaculator/NetprofitcaculatorApplication.java
@@ -0,0 +1,12 @@
+package com.netprofitcalculator.netprofitcaculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetprofitcaculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetprofitcaculatorApplication.class, args);
+	}
+}
